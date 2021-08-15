1c1a5774b396be785820b60779198079ece5a0cc
Safraz Zamdin <mohamedzamdin@gmail.com>
2021-08-15 10:25:38 -0400

    Updated to use docker volme

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./glucose.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////opt/glucose/glucose.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
